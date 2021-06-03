6c0d2dcb9a08d80d84b08e8b6a006436d6f7a8b1
Pablo Werlang <pswerlang@gmail.com>
2021-06-03 07:47:04 +0000

    Server now collect prices for statistics. Fixed uncaught exceptions error.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const fs = require('fs');
 
 const app = express();
-const port = 4201;
+const port = 4200;
 
 const mysqlConnection = mysql.createConnection(JSON.parse(fs.readFileSync(__dirname  + '/mysql_config.json')));
 
@@ -22,9 +22,7 @@ const corsOptions = {
 };
 
 app.get('/gas', cors(corsOptions), (req, res) => {
-    // request('127.0.0.1:8097').then(resp => resp.json().then(data => {
-    request('http://127.0.0.1:8097', (error, response, data) => {
-        // sample data: {"safeLow":5.0,"standard":5.0,"fast":5.0,"fastest":5.0,"block_time":15,"blockNum":7499408}
+    requestOracle().then(({error, response, data}) => {
         if (error){
             res.send({ error: error });
         }
@@ -38,8 +36,16 @@ app.get('/gas', cors(corsOptions), (req, res) => {
                 imediate: data.fastest
             };
 
-            const sql = `INSERT INTO api_requests (ip, origin, response) VALUES ('${req.ip}', '${req.hostname}', '${JSON.stringify(resp)}')`;
-            mysqlConnection.query(sql);
+            // save API request to DB for statistics purpose
+            try {
+                mysqlConnection.execute(`INSERT INTO api_requests (ip, origin) VALUES (?, ?)`, [
+                    `${req.header('x-real-ip') || ''}`,
+                    `${req.header('Origin') || ''}`,
+                ]);
+            }
+            catch(err) {
+                console.log(err);
+            }
 
             res.send(resp);
         }
@@ -51,3 +57,35 @@ app.use(express.static(__dirname + '/public/'));
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
+
+async function requestOracle(){
+    return new Promise(resolve => {
+        request('http://127.0.0.1:8097', (error, response, data) => {
+            // sample data: {"safeLow":5.0,"standard":5.0,"fast":5.0,"fastest":5.0,"block_time":15,"blockNum":7499408}
+            resolve({ error: error, response: response, data: data });
+        });
+    });
+}
+
+// get prices to build database with price history
+async function buildHistory(){
+    const oracle = await requestOracle();
+    if (oracle.data){
+        const data = JSON.parse(oracle.data);
+
+        try {
+            mysqlConnection.execute(`INSERT INTO price_history (imediate, fast, standard, slow) VALUES (?, ?, ?, ?)`, [
+                data.fastest,
+                data.fast,
+                data.standard,
+                data.safeLow,
+            ]);
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+
+    setTimeout(() => buildHistory(), 1000 * 60); // 1 minute
+}
+buildHistory();
